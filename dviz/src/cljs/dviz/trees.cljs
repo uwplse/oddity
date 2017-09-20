24875ac16c68c07026cdf9595cc4baf84592c1a3
Doug Woos <doug.woos@gmail.com>
2017-09-20 14:48:52 -0700

    fix trees

--- dviz/src/cljs/dviz/trees.cljs
+++ dviz/src/cljs/dviz/trees.cljs
@@ -12,21 +12,33 @@
 (defn update-nth [l n f & args]
   (concat (take n l) (list (apply f (nth l n) args)) (drop (inc n) l)))
 
-(defn tree-append [tree zipper v]
-  (if (empty? zipper)
-    (cons (root tree) (concat (children tree) (list (leaf v))))
-    (cons (root tree) (update-nth (children tree) (first zipper)
-                                  tree-append (rest zipper) v))))
+(defn set-nth [l n x]
+  (concat (take n l) (list x) (drop (inc n) l)))
 
-(defn tree-get [tree zipper]
-  (if (empty? zipper)
-    (root tree)
-    (recur (nth (children tree) (first zipper)) (rest zipper))))
+(defn append-path
+  "Takes a tree, a path and a value. Returns a new tree with that
+  value appended at the position identified by the path and a new
+  path that identifies the appended value's position"
+  [tree path v]
+  (if (empty? path)
+    [(cons (root tree) (concat (children tree) (list (leaf v))))
+     [(count (children tree))]]
+    (let [n (first path)
+          path' (rest path)
+          subtree (nth (children tree) n)
+          [subtree' path'] (append-path subtree path' v)]
+      [(cons (root tree) (set-nth (children tree) n subtree'))
+       (cons n path')])))
 
-(defn tree-layout
+(defn get-path [tree path]
+  (if (empty? path)
+    tree
+    (recur (nth (children tree) (first path)) (rest path))))
+
+(defn layout
   "Takes a tree and layes it out, returning a list of maps:
-  {:position [<x> <y>] :value <v> :path <zipper>}"
-  ([tree dx dy] (tree-layout tree dx dy 0 0 [] nil))
+  {:position [<x> <y>] :value <v> :path <path>}"
+  ([tree dx dy] (layout tree dx dy 0 0 [] nil))
   ([tree dx dy x y path parent]
    (let [root-layout {:position [x y] :value (root tree) :path path :parent parent}]
      (cons root-layout
@@ -37,4 +49,4 @@
                          new-x (+ x dx)
                          new-y (+ y (* index dy))
                          new-path (conj path index)]]
-                    (tree-layout child dx dy new-x new-y new-path [x y])))))))
+                    (layout child dx dy new-x new-y new-path [x y])))))))
