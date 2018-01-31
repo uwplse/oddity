d5f342b69ac3c5b471cf88ad8b3d57f91c0f34fd
Doug Woos <doug.woos@gmail.com>
2018-01-31 13:03:33 -0800

    bust the cache

--- dviz/env/prod/clj/dviz/middleware.clj
+++ dviz/env/prod/clj/dviz/middleware.clj
@@ -1,5 +1,17 @@
 (ns dviz.middleware
   (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
 
+(defn bust-cache-response [response]
+  (assoc-in response [:headers "Cache-control"] "no-store"))
+
+(defn wrap-bust-cache [handler]
+  (fn
+    ([request]
+     (bust-cache-response (handler request)))
+    ([request respond raise]
+      (handler request #(respond (bust-cache-response %)) raise))))
+
 (defn wrap-middleware [handler]
-  (wrap-defaults handler site-defaults))
+  (-> handler
+      (wrap-defaults site-defaults)
+      (wrap-bust-cache)))
