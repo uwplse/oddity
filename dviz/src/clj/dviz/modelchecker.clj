522022c2e1f15a2dcbf937e621c92b5240dd23e0
Doug Woos <doug.woos@gmail.com>
2018-10-18 16:58:10 -0700

    A working dfs checker!

--- dviz/src/clj/dviz/modelchecker.clj
+++ dviz/src/clj/dviz/modelchecker.clj
@@ -8,28 +8,55 @@
   (run! [this action] "Run action")
   (matches? [this pred] "Does this state match a predicate?"))
 
+
+(defrecord CoolNumberProblem [x y]
+  IState
+  (restart! [this] (->CoolNumberProblem 0 0))
+  (actions [this] [:inc-x :inc-y])
+  (run! [this action]
+    (if (= action :inc-x)
+      (->CoolNumberProblem (inc x) y)
+      (->CoolNumberProblem x (inc y))))
+  (matches? [this pred] (pred x y)))
+
 (defn prefix? [a b]
   (if (<= (count a) (count b))
     (= a (take (count a) b))
     false))
 
+(defn add-actions-to-worklist [state current worklist]
+  (concat (map #(conj current %) (actions state)) worklist))
+
 (defn dfs
-  ([state pred] (dfs state pred 3))
-  ([state pred delta-depth]
-   (dfs state pred delta-depth [] () 3))
-  ([state pred delta-depth current worklist depth]
-   (if (matches? state pred)
-     {:result :found :trace current}
-     (let [worklist (concat (map #(conj current %) (actions state)))]
-       (if (empty? worklist)
-         {:result :not-found}
-         (let [next (first worklist)]
-           (if (prefix? current next)
-             ;; this is the fast path
-             (do
-               (doseq [a (drop (count current) next)]
-                 (run! state a))
-               (recur state pred delta-depth next (rest worklist) depth))
-             (do
-               (restart! state)
-               (recur state pred delta-depth [] worklist depth)))))))))
+  ([state pred max-depth] (dfs state pred max-depth 3))
+  ([state pred max-depth delta-depth]
+   (loop [state state
+          depth delta-depth
+          worklist (add-actions-to-worklist state [] ())
+          next-worklist ()
+          current []]
+     (cond
+       (and (empty? worklist) (or (empty? next-worklist) (> (+ depth delta-depth) max-depth)))
+       {:result :not-found}
+       
+       (empty? worklist)
+       (recur state (+ depth delta-depth) next-worklist () current)
+
+       :else
+       (let [next (first worklist)]
+         (if (prefix? current next)
+           (let [state (reduce run! state (drop (count current) next))]
+             (if (matches? state pred)
+               {:result :found :trace next}
+               (if (< (count next) depth)
+                 (recur state
+                        depth
+                        (add-actions-to-worklist state next (rest worklist))
+                        next-worklist
+                        next)
+                 (recur state
+                        depth
+                        (rest worklist)
+                        (add-actions-to-worklist state next next-worklist)
+                        next))))
+           (recur (restart! state) depth worklist next-worklist [])))))))
