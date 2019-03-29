ba0a6d6c59037e48aa56aca52326d59b94665cff
Doug Woos <doug.woos@gmail.com>
2019-03-29 16:55:17 -0700

    Fix model checker tests

--- oddity/test/clj/oddity/dsmodelchecker_test.clj
+++ oddity/test/clj/oddity/dsmodelchecker_test.clj
@@ -5,8 +5,9 @@
 
 
 (deftest new-state-test
-  (let [res (new-state {:responses [["A" {:set-timeouts [{:type "tA" :body "B"}]
-                                          :send-messages [{:type "mA" :body "B" :to "nodeB"}]
+  (let [res (new-state {:responses [["A" {:set-timeouts [{:type "tA" :body "B" :to "A"}]
+                                          :send-messages [{:type "mA" :body "B" :to "nodeB"
+                                                           :from "A"}]
                                           :state-updates [{:path ["field" "subfield"]
                                                            :value "value"}]}]]})]
     (is (= (:timeouts res) [{:to "A" :type "tA" :body "B"}]))
@@ -14,9 +15,11 @@
     (is (= (:states res) {"A" {"field" {"subfield" "value"}}}))))
 
 (deftest new-state-with-raw-state-test
-  (let [res (new-state {:responses [["A" {:set-timeouts [{:type "tA" :body "B"}]
-                                          :send-messages [{:type "mA" :body "B" :to "nodeB"}]
-                                          :states {"A" {"field" {"subfield" "value"}} "B" {"otherfield" "othervalue"}}}]]})]
+  (let [res (new-state {:responses [["A" {:set-timeouts [{:type "tA" :body "B" :to "A"}]
+                                          :send-messages [{:type "mA" :body "B" :to "nodeB"
+                                                           :from "A"}]
+                                          :states {"A" {"field" {"subfield" "value"}}
+                                                   "B" {"otherfield" "othervalue"}}}]]})]
     (is (= (:timeouts res) [{:to "A" :type "tA" :body "B"}]))
     (is (= (:messages res) [{:to "nodeB" :type "mA" :body "B" :from "A"}]))
     (is (= (:states res) {"A" {"field" {"subfield" "value"}}}))))
@@ -71,13 +74,14 @@
                               [{:path ["pings"] :value pings}])))))
       (restart-system! [this]
           (reset! st {"node1" {:timeouts 0 :pings 0} "node2" {:timeouts 0 :pings 0}})
-          {:responses [["node1" {:set-timeouts [{:type "timeout" :body {}}]
-                                 :send-messages [{:to "node2" :type "ping" :body {}}]
+          {:responses [["node1" {:set-timeouts [{:type "timeout" :body {} :to "node1"}]
+                                 :send-messages [{:to "node2" :type "ping" :body {}
+                                                  :from "node1"}]
                                  :state-updates [{:path ["timeouts"]
                                                   :value 0}
                                                  {:path ["pings"]
                                                   :value 0}]}]
-                       ["node2" {:set-timeouts [{:type "timeout" :body {}}]
+                       ["node2" {:set-timeouts [{:type "timeout" :body {} :to "node2"}]
                                  :send-messages []
                                  :state-updates [{:path ["timeouts"]
                                                   :value 0}
